7d65bcc68657571f359ad1230d601ea9b6f254d2
Chrispus Ngure <chrispus.ngure@gmail.com>
2018-12-11 07:12:17 +0300

    add name= to function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $("form#quiz").submit(function(event){
 
-    var questionOne = $("input:radio[questionOne]:checked").val();
-    var questionTwo = $("input:radio[questionTwo]:checked").val();
-    var questionThree = $("input:radio[questionThree]:checked").val();
-    var questionFour = $("input:radio[questionFour]:checked").val();
-    var questionFive = $("input:radio[questionFive]:checked").val();
+    var questionOne = $("input:radio[name=questionOne]:checked").val();
+    var questionTwo = $("input:radio[name=questionTwo]:checked").val();
+    var questionThree = $("input:radio[name=questionThree]:checked").val();
+    var questionFour = $("input:radio[name=questionFour]:checked").val();
+    var questionFive = $("input:radio[name=questionFive]:checked").val();
     var result = parseInt(questionOne) + parseInt(questionTwo) + parseInt(questionThree) + parseInt(questionFour) + parseInt(questionFive);
     $("#result").text("You Score: " + result + "%");
 
