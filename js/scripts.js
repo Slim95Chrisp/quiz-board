5b8b3481b41c98c0dc200da51335295c3c32fe4a
Chrispus Ngure <chrispus.ngure@gmail.com>
2018-12-11 12:53:59 +0300

    edit function

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,16 @@
-$(document).ready(function() {
-  $("form#quiz").submit(function(event) {
+$(document).ready(function(){
+ $("#result").click(function(event){
 
-    var questionOne = $("input:radio[name=questionOne]:checked").val();
-    var questionTwo = $("input:radio[name=questionTwo]:checked").val();
-    var questionThree = $("input:radio[name=questionThree]:checked").val();
-    var questionFour = $("input:radio[name=questionFour]:checked").val();
-    var questionFive = $("input:radio[name=questionFive]:checked").val();
-    var result = parseInt(questionOne) + parseInt(questionTwo) +
-      parseInt(questionThree) + parseInt(questionFour) + parseInt(questionFive);
-    $("#result").text("You Score: " + result + "%");
-
-    $("button").click(function() {
-      $("#result-showing").toggle();
-    })
-    $("form#quiz").show();
-    $("#result").show();
-    event.preventDefault();
-  });
+   var questionOne = $("input:radio[name=questionOne]:checked").val();
+   var questionTwo = $("input:radio[name=questionTwo]:checked").val();
+   var questionThree = $("input:radio[name=questionThree]:checked").val();
+   var questionFour = $("input:radio[name=questionFour]:checked").val();
+   var questionFive = $("input:radio[name=questionFive]:checked").val();
+   var result = parseInt(questionOne) + parseInt(questionTwo) +
+   parseInt(questionThree) + parseInt(questionFour) + parseInt(questionFive);
+   $("#result").text("You Score: " + result + "%");
+   $("#result").show();
+   $("#quiz").hide();
+   event.preventDefault();
+ });
 });
